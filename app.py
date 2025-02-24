5540532d195daf332cb2da66d2bca5c0c1d78933
546892172 <546892172@qq.com>
2025-02-24 17:10:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from os import path
 from pathlib import Path
-
 from flask import Flask, render_template
 from flask_frozen import Freezer
-
 
 template_folder = path.abspath('./wiki')
 
 app = Flask(__name__, template_folder=template_folder)
-#app.config['FREEZER_BASE_URL'] = environ.get('CI_PAGES_URL')
 app.config['FREEZER_DESTINATION'] = 'public'
 app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
@@ -18,18 +15,13 @@ freezer = Freezer(app)
 def freeze():
     freezer.freeze()
 
-@app.cli.command()
-def serve():
-    freezer.run()
-
 @app.route('/')
 def home():
-    return render_template('pages/home.html')
+    return render_template('pages/home.html')  # 从 wiki/pages 目录加载首页
 
 @app.route('/<page>')
 def pages(page):
-    return render_template(str(Path('pages')) + '/' + page.lower() + '.html')
+    return render_template(f'pages/{page}.html')  # 访问 wiki/pages 下的其他页面
 
-# Main Function, Runs at http://0.0.0.0:8080
 if __name__ == "__main__":
     app.run(port=8080)
